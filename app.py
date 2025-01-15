16efbe54160d8935b115e762abf88510d4dceb1d
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-01-15 11:47:43 +0500

    feat: aiogram

--- app.py
+++ app.py
@@ -1,41 +1,16 @@
-import asyncio
-import types
+from aiogram import Bot, Dispatcher, types
+from asyncio import run
 
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.filters import Command
-from aiogram.types import Message
-
-API_TOKEN = "6667385868:AAEgEGKSM_YoHyGBAd2Xf4JwBt8tRwen6U8"
-
-# bot and Dispatcher settings
-bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# create router
-router = Router()
+async def echo(message: types.Message, bot:Bot):
+    await message.copy_to(chat_id=message.chat.id)
+    # await message.reply(message.text)
 
 
-@router.message(Command("start"))
-async def start_handler(message: types.Message):
-    await message.answer("Assalomu alaykum, EchoBotga xush kelibsiz")
-
-@router.message(Command("help"))
-async def help_handler(message: types.Message):
-    await message.answer("Botdan foydalanish uchun biror nima yozing!")
-
-@router.message(Command("about"))
-async def about_handler(message: types.Message):
-    await message.answer("Bu bot shunchaki test uchun yaratildi!")
-
-@router.message()
-async def echo(message: Message):
-    await message.answer(message.text)
-
-dp.include_router(router)
-
-async def main():
-    print("Bot ishga tushdi")
+async def start():
+    dp.message.register(echo)
+    bot = Bot("6667385868:AAEgEGKSM_YoHyGBAd2Xf4JwBt8tRwen6U8")
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+run(start())
---
+++ echo.py
@@ -0,0 +1,43 @@
+import asyncio
+import types
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+API_TOKEN = "6667385868:AAEgEGKSM_YoHyGBAd2Xf4JwBt8tRwen6U8"
+
+# bot and Dispatcher settings
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+# create router
+router = Router()
+
+
+@router.message(Command("start"))
+async def start_handler(message: types.Message):
+    await message.answer("Assalomu alaykum, EchoBotga xush kelibsiz")
+
+@router.message(Command("help"))
+async def help_handler(message: types.Message):
+    await message.answer("Botdan foydalanish uchun biror nima yozing!")
+
+@router.message(Command("about"))
+async def about_handler(message: types.Message):
+    await message.answer("Bu bot shunchaki test uchun yaratildi!")
+
+@router.message()
+async def echo(message: Message):
+    await message.answer(message.text)
+
+dp.include_router(router)
+
+async def main():
+    print("Bot ishga tushdi")
+    await dp.start_polling(bot)
+
+# async def call_back_command(callback_query):
+
+if __name__ == "__main__":
+    asyncio.run(main())
